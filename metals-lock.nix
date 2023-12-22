c57981258a674d4644ebcf87b53e313a7f2da116
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-12-22 11:28:47 +0000

    Update Metals to 1.2.0

--- metals-lock.nix
+++ metals-lock.nix
@@ -1,4 +1,4 @@
 {
-  "version" = "1.1.0";
-  "outputHash" = "sha256-9zigJM0xEJSYgohbjc9ZLBKbPa/WGVSv3KVFE3QUzWE=";
+  "version" = "1.2.0";
+  "outputHash" = "sha256-nikQ/GFRWmYYzboc9TWIi9gd5kwgCxOLhvIEQWusFik=";
 }
