d92020282058ac7fed0d0c1b9675d6937d2e34d1
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-04-13 09:04:24 +0000

    Update Metals to 1.5.2+40-28dd5d77-SNAPSHOT

--- metals-lock.nix
+++ metals-lock.nix
@@ -1,4 +1,4 @@
 {
-  "version" = "1.5.2+25-3c71e7ea-SNAPSHOT";
-  "outputHash" = "sha256-nt8SxwmyuX3YOo1PlcfRmRUUS93yXQNnp+B0gU/w9qY=";
+  "version" = "1.5.2+40-28dd5d77-SNAPSHOT";
+  "outputHash" = "sha256-sqBEJWRSEo2KxIqX96EUPMztKwXQ3CmXvy+C+FS0RIE=";
 }
