707bbdde29f3f8200a03fccbcdc4d7a8a8ec60d1
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2026-04-25 14:30:40 +0200

    Another approach to propagate jvm args

--- metals-builder.nix
+++ metals-builder.nix
@@ -21,7 +21,7 @@ pkgs.stdenv.mkDerivation rec {
     outputHashAlgo = "sha256";
   };
 
-  nativeBuildInputs = with pkgs; [ setJavaClassPath ];
+  nativeBuildInputs = with pkgs; [ makeWrapper setJavaClassPath ];
   buildInputs = [ deps ];
 
   dontUnpack = true;
@@ -31,29 +31,9 @@ pkgs.stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/bin
 
-    cat > $out/bin/metals <<EOF
-    #!${pkgs.runtimeShell}
-    set -eu
-
-    java_args="${extraJavaOpts}"
-    app_args=""
-
-    while [ "$#" -gt 0 ]; do
-      case "$1" in
-        -D*|--add-opens=*|--add-exports=*|-X*|-XX:*|-XX+*|-agentlib:*|-javaagent:*|-verbose*|-ea|-da|-esa|-dsa)
-          java_args="$java_args '$1'"
-          ;;
-        *)
-          app_args="$app_args '$1'"
-          ;;
-      esac
-      shift
-    done
-
-    eval "exec ${pkgs.jre}/bin/java $java_args -cp '$CLASSPATH' scala.meta.metals.Main $app_args"
-    EOF
-
-    chmod +x $out/bin/metals
+    makeWrapper ${pkgs.jre}/bin/java $out/bin/metals \
+      --set-default JAVA_OPTS "${extraJavaOpts}" \
+      --add-flags "\$JAVA_OPTS -cp $CLASSPATH scala.meta.metals.Main"
   '';
 
   meta = with pkgs.lib; {
